7c5150c6f2a87a6b97364b6306a0163074bb4f52
Yash Agarwal <yash00000agarwal@gmail.com>
2025-02-04 23:02:22 +0530

    WIP: Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => {
-	return <div className="home-container"></div>;
+	return (
+		<div className="home-container">
+			<div>Home Image</div>
+		</div>
+	);
 };
 
 export default Home;
