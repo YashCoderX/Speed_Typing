eaa7ab9218b6b8c179318b8c6dbaecab7928d570
Yash Agarwal <yash00000agarwal@gmail.com>
2025-02-05 12:40:47 +0530

    Completed building Home page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Nav from '../Nav/Nav';
+import Home from '../Home/Home';
 
 class App extends React.Component {
 	render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
 				<Nav />
 
 				{/* {Landing section} */}
+				<Home />
 
 				{/* {Challenge section} */}
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import './Home.css';
+import flash from './../../assets/flash.png';
+import TypeWriter from 'typewriter-effect';
 
 const Home = () => {
 	return (
 		<div className="home-container">
-			<div>Home Image</div>
+			<div data-aos="fade-right" className="home-left">
+				<h1 className="home-header">Can you type...</h1>
+				<div className="typewrite-container">
+					<TypeWriter
+						options={{
+							strings: ['Fast?', 'Correct?', 'Quick?'],
+							autoStart: true,
+							loop: true,
+						}}
+					/>
+				</div>
+			</div>
+			<div className="home-right">
+				<img
+					data-aos="fade-left"
+					className="flash-image"
+					src={flash}
+					alt="hero"
+				/>
+			</div>
 		</div>
 	);
 };
